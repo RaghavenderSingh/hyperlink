3cd01ffc1c99eba7486201c5fddd2e831de87794
Sai Karthik <tsaikarthik@yahoo.in>
2024-08-19 00:04:47 +0530

    Implemented Search and used api instead of supported tokens and onClick handler is pending

--- components/TokenListDialog.tsx
+++ components/TokenListDialog.tsx
@@ -1,7 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { SUPPORTED_TOKENS, TokenDetails } from "@/lib/tokens";
+import { useFetchTokens, TokenApiList } from "@/app/hooks/useFetchTokens";
+import {
+  Command,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
 
 export function TokenListDialog({
   selectedToken,
@@ -16,8 +22,9 @@ export function TokenListDialog({
     setLocalSelectedToken(selectedToken);
   }, [selectedToken]);
   const [open, setOpen] = useState(false);
+  const { tokens, loading, error } = useFetchTokens()
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
+    <Dialog open={open} onOpenChange={setOpen} >
       <DialogTrigger asChild>
         <Button onClick={() => setOpen(true)}>
           <img src={localSelectedToken?.image} alt={localSelectedToken?.name} className="w-6 h-6 mr-2"></img>
@@ -42,23 +49,92 @@ export function TokenListDialog({
         </Button>
       </DialogTrigger>
       <DialogContent className="width-full">
-        <div className="my-4">
-          {SUPPORTED_TOKENS.map((token) => (
-            <div
-              key={token.name}
-              onClick={() => {
-                setLocalSelectedToken(token);
-                onSelect(token);
-                setOpen(false);
-              }}
-              className="cursor-pointer hover:bg-gray-200 text-left pl-4 pb-4 pt-4 flex items-start gap-2"
-            >
-              <img src={token.image} alt={token.name} className="w-6 h-6" />
-              {token.name}
-            </div>
-          ))}
-        </div>
+        {tokens && <AllTokensComponent tokens={tokens} />}
       </DialogContent>
-    </Dialog>
+    </Dialog >
   );
+}
+
+
+const AllTokensComponent = ({ tokens }: { tokens: TokenApiList[] }) => {
+  const tokenRef = useRef<HTMLDivElement>(null);
+  const sentinelRef = useRef<HTMLDivElement>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentTokens, setCurrentTokens] = useState(20);
+  const [searchQuery, setSearchQuery] = useState<string | null>(null)
+  const [debouncedTokenInput, setDebouncedTokenInput] = useState<string>('');
+  const [searchedTokens, setSearchedTokens] = useState<TokenApiList[] | null>(null);
+
+
+  useEffect(() => {
+    if (searchQuery === "") {
+      setSearchedTokens(tokens)
+    }
+    const handler = setTimeout(() => {
+      searchQuery && setDebouncedTokenInput(searchQuery);
+    }, 1000);
+
+    // Cleanup function to clear the timeout if inputValue changes before the timeout completes
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [searchQuery]);
+
+  useEffect(() => {
+    if (debouncedTokenInput) {
+      const searchedTokens = tokens.filter(token =>
+        token.name.toLowerCase().includes(debouncedTokenInput.toLowerCase())
+      );
+      setSearchedTokens(searchedTokens)
+    }
+    console.log("debouncedTokenInput: " + debouncedTokenInput)
+  }, [debouncedTokenInput]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const entry = entries[0]
+      if (entry.isIntersecting) {
+        setCurrentPage(prevPage => prevPage + 1)
+      }
+    })
+
+    if (sentinelRef.current) {
+      observer.observe(sentinelRef.current);
+    }
+
+    return () => {
+      if (sentinelRef.current) {
+        observer.unobserve(sentinelRef.current);
+      }
+    };
+  }, [])
+
+  useEffect(() => {
+    setCurrentTokens(20 * currentPage);
+  }, [currentPage])
+
+  return (
+    <Command>
+      <div className="mb-1 mt-4 border shadow-md">
+        <input className="w-full rounded-lg p-2" onChange={(e) => setSearchQuery(e.target.value)} placeholder="Type a command or search..." />
+      </div>
+      <div ref={tokenRef} className="" >
+        <CommandList className="border shadow-md">
+          {searchedTokens ? searchedTokens.slice(0, currentTokens).map((token) => (
+            <CommandItem key={token.address}>
+              <img src={token.logoURI} alt={token.name} className="w-6 h-6 mr-2" />
+              {token.name}
+            </CommandItem>
+
+          )) : tokens && tokens.slice(0, currentTokens).map((token) => (
+            <CommandItem key={token.address}>
+              <img src={token.logoURI} alt={token.name} className="w-6 h-6 mr-2" />
+              {token.name}
+            </CommandItem>
+
+          ))}
+          <div ref={sentinelRef} />
+        </CommandList>
+      </div>
+    </Command>)
 }
