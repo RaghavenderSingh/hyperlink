939d9d66b847f9775b0248d0b11b19c9e1cca450
Tumu Sai Karthik <karthiktumusai@gmail.com>
2024-08-22 18:44:30 +0530

    responsive design final

--- app/[hash]/page.tsx
+++ app/[hash]/page.tsx
@@ -241,8 +241,16 @@ const HyperLinkCard: React.FC = () => {
                 <div className="flex-col items-start justify-start rounded-full bg-grey-50 xs:p-3">
                   <div className="text-left">Recreate this Hyperlink</div>
                   <div className="text-left text-xs font-normal text-grey-500">
-                    Move the entire value to a new Hyperlink so only you have
-                    the link.
+                    <div>
+                      <div className="block md:hidden">
+                        <div>Move the entire value to a new</div>
+                        <div>Hyperlink so only you have the link.</div>
+                      </div>
+                      <div className="hidden md:block">
+                        Move the entire value to a new Hyperlink so only you
+                        have the link.
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -259,7 +267,13 @@ const HyperLinkCard: React.FC = () => {
                 <div className="flex-col items-start justify-start rounded-full bg-grey-50 xs:p-3">
                   <div className="text-left">Withdraw to your wallet</div>
                   <div className="text-left text-xs font-normal text-grey-500">
-                    Withdraw the entire value to your connected wallet.
+                    <div className="block md:hidden">
+                      <div>Withdraw the entire value to your </div>
+                      <div>connected wallet.</div>
+                    </div>
+                    <div className="hidden md:block">
+                      Withdraw the entire value to your connected wallet.
+                    </div>
                   </div>
                 </div>
               </div>
@@ -329,137 +343,95 @@ const HyperLinkCard: React.FC = () => {
             </CardTitle>
           </CardHeader>
 
-      <CardContent className="space-y-4">
-        <p className="text-lg font-thin text-center">
-          The link to this page contains this value. Make sure you don't lose
-          it!
-        </p>
-        <div className="flex justify-center items-center">
-          <Badge className="text-base sm:text-lg md:text-xl p-2 sm:p-3 md:p-4 px-3 sm:px-4 md:px-5">
-            {url.href}
-          </Badge>
-        </div>
-        <div className="flex justify-center items-center gap-3">
-          <Button onClick={handleCopy} className="h-[80px] flex-1">
-            <div className="flex flex-col items-center">
-              <Copy />
-              <span>Copy</span>
+          <CardContent className="space-y-4">
+            <p className="text-lg font-thin text-center">
+              The link to this page contains this value. Make sure you don't
+              lose it!
+            </p>
+            <div className="flex justify-center items-center">
+              <Badge className="text-base sm:text-lg md:text-xl p-2 sm:p-3 md:p-4 px-3 sm:px-4 md:px-5">
+                {url.href}
+              </Badge>
             </div>
-          </Button>
-          <Button onClick={handleBookmark} className="h-[80px] flex-1">
-            <div className="flex flex-col items-center">
-              <Bookmark />
-              <span>Bookmark</span>
+            <div className="flex justify-center items-center gap-3">
+              <Button onClick={handleCopy} className="h-[80px] flex-1">
+                <div className="flex flex-col items-center">
+                  <Copy />
+                  <span>Copy</span>
+                </div>
+              </Button>
+              <Button onClick={handleBookmark} className="h-[80px] flex-1">
+                <div className="flex flex-col items-center">
+                  <Bookmark />
+                  <span>Bookmark</span>
+                </div>
+              </Button>
+              <Button className="h-[80px] flex-1">
+                <div className="flex flex-col items-center">
+                  <QrCodeIcon />
+                  <span>QR code</span>
+                </div>
+              </Button>
             </div>
-          </Button>
-          <Button className="h-[80px] flex-1">
-            <div className="flex flex-col items-center">
-              <QrCodeIcon />
-              <span>QR code</span>
-            </div>
-          </Button>
-        </div>
-        {balance !== null && (
-          <div
-            className="rounded-xl shadow-lg p-6 flex flex-row justify-between"
-            style={{ backgroundImage: `url(${background.src})` }}
-          >
-            <div className="flex flex-col items-start">
-              <div className="mt-6 sm:mt-8 md:mt-10 lg:mt-12 xl:mt-16">
-                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight md:leading-normal font-bold">
-                  Your
-                </h1>
-                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight md:leading-normal font-bold">
-                  Balance
-                </h1>
+            {balance !== null && (
+              <div
+                className="rounded-xl shadow-lg p-6 flex flex-row justify-between"
+                style={{ backgroundImage: `url(${background.src})` }}
+              >
+                <div className="flex flex-col items-start">
+                  <div className="mt-6 sm:mt-8 md:mt-10 lg:mt-12 xl:mt-16">
+                    <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight md:leading-normal font-bold">
+                      Your
+                    </h1>
+                    <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl leading-tight md:leading-normal font-bold">
+                      Balance
+                    </h1>
+                  </div>
+                  <div className="mt-6 sm:mt-8 md:mt-10 lg:mt-12 xl:mt-16">
+                    <Badge className="text-lg">{url.hash}</Badge>
+                  </div>
+                </div>
+                <div>
+                  <div className="text-center mt-6 sm:mt-8 md:mt-10 lg:mt-12 xl:mt-16">
+                    <p className="text-pink-400">{balance.toFixed(4)} SOL</p>
+                    {usdBalance && (
+                      <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
+                        ${usdBalance.toFixed(2)}
+                      </h1>
+                    )}
+                  </div>
+                  <div className="flex justify-end mt-10 sm:mt-6 md:mt-8 lg:mt-14 xl:mt-[7rem] ">
+                    <p className="text-gray-400 text-xs">
+                      POWERED BY Hyperlink
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div className="mt-6 sm:mt-8 md:mt-10 lg:mt-12 xl:mt-16">
-                <Badge className="text-lg">{url.hash}</Badge>
-              </div>
-            </div>
-            <div>
-              <div className="text-center mt-6 sm:mt-8 md:mt-10 lg:mt-12 xl:mt-16">
-                <p className="text-pink-400">{balance.toFixed(4)} SOL</p>
-                {usdBalance && (
-                  <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl">
-                    ${usdBalance.toFixed(2)}
-                  </h1>
-                )}
-              </div>
-              <div className="flex justify-end mt-10 sm:mt-6 md:mt-8 lg:mt-14 xl:mt-[7rem] ">
-                <p className="text-gray-400 text-xs">POWERED BY Hyperlink</p>
-              </div>
-            </div>
-          </div>
-        )}
-        <div className="flex justify-center items-center gap-3">
-          <Button className="w-full">Send</Button>
-          <Button onClick={() => setShowQrModal(true)} className="w-full">
-            Receive
-          </Button>
+            )}
+            <div className="flex justify-center items-center gap-3">
+              <Button onClick={() => setStep(1)} className="w-full">
+                Send
+              </Button>
+              <Button onClick={() => setShowQrModal(true)} className="w-full">
+                Receive
+              </Button>
 
-          {showQrModal && hyperlink?.keypair?.publicKey && (
-            <WalletModal
-              isVisible={showQrModal}
-              onClose={() => setShowQrModal(false)}
-              publicKey={hyperlink?.keypair?.publicKey.toBase58().toString()}
-            />
-          )}
-        </div>
-        <Separator className="my-10" />
-        <div className="flex w-full flex-col justify-start space-y-5 xs:space-y-0 xs:flex-row xs:space-x-10">
-          <Button
-            onClick={createLinkAndTransfer}
-            className="flex justify-between text-base font-medium p-10 xs:w-full xs:text-base"
-          >
-            <div className="flex items-start justify-start gap-2 sm:gap-4 md:gap-6 lg:gap-8 xl:gap-[10px]">
-              <div className="flex items-center justify-center align-middle mt-2 rounded-full bg-grey-50 xs:p-3">
-                <Wallet />
-              </div>
-              <div className="flex-col items-start justify-start rounded-full bg-grey-50 xs:p-3">
-                <div className="text-left">Recreate this Hyperlink</div>
-                <div className="text-left text-xs font-normal text-grey-500">
-                  <div>
-                    <div className="block md:hidden">
-                      <div>Move the entire value to a new</div>
-                      <div>Hyperlink so only you have the link.</div>
-                    </div>
-                    <div className="hidden md:block">
-                      Move the entire value to a new Hyperlink so only you have
-                      the link.
-                    </div>
-                  </div>
-                </div>
-              </div>
+              {showQrModal && hyperlink?.keypair?.publicKey && (
+                <WalletModal
+                  isVisible={showQrModal}
+                  onClose={() => setShowQrModal(false)}
+                  publicKey={hyperlink?.keypair?.publicKey
+                    .toBase58()
+                    .toString()}
+                />
+              )}
             </div>
-            {isLoading ? <Spinner /> : <ChevronRight />}
-          </Button>
-          <Button
-            onClick={handleTransfer}
-            className="flex justify-between text-base font-medium p-10 xs:w-full xs:text-base"
-          >
-            <div className="flex items-start justify-start gap-[10px]">
-              <div className="flex items-center justify-center align-middle mt-2 rounded-full bg-grey-50 xs:p-3">
-                <Wallet />
-              </div>
-              <div className="flex-col items-start justify-start rounded-full bg-grey-50 xs:p-3">
-                <div className="text-left">Withdraw to your wallet</div>
-                <div className="text-left text-xs font-normal text-grey-500">
-                  <div className="block md:hidden">
-                    <div>Withdraw the entire value to your </div>
-                    <div>connected wallet.</div>
-                  </div>
-                  <div className="hidden md:block">
-                    Withdraw the entire value to your connected wallet.
-                  </div>
-                </div>
-              </div>
-            </div>
-            <ChevronRight />
-          </Button>
-        </div>
-      </CardContent>
-    </Card>
+            <Separator className="my-10" />
+            <div>{handleSteps(step)}</div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
   );
 };
 
