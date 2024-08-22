8827a7206eb1b3eb2897fedbb89a886d66825694
Raghavender Singh <92844911+RaghavenderSingh@users.noreply.github.com>
2024-08-22 10:30:59 +0530

    clint side of hyperlink

---
+++ components/linkAsWallet/SendHyperlink.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Button } from "../ui/button";
+import { ArrowLeft, ChevronRight, KeyRound, Link2 } from "lucide-react";
+import CustomTextField from "../CustomTextField";
+type FundingOptionsProps = {
+  setStep: (value: number) => void;
+  setTransferAmount: (value: string) => void;
+};
+export default function ({ setStep, setTransferAmount }: FundingOptionsProps) {
+  return (
+    <div className="flex w-full flex-col justify-start space-y-5 xs:space-y-0 xs:flex-row xs:space-x-10">
+      <div
+        onClick={() => setStep(1)}
+        className="mb-3.5 mr-auto flex w-max cursor-pointer items-center justify-start gap-1 text-sm font-semibold text-grey-700 hover:opacity-70"
+      >
+        <span>
+          <ArrowLeft className="w-4 h-4" />
+        </span>{" "}
+        <span>Back</span>
+      </div>
+      <div className="flex w-full flex-col justify-start space-y-5 xs:space-y-0 xs:flex-row xs:space-x-10">
+        <CustomTextField setAmount={setTransferAmount} />
+      </div>
+    </div>
+  );
+}
