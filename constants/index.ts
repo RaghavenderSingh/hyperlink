64c8235d652432b74249c30de84c14430a7d2855
Raghavender Singh <92844911+RaghavenderSingh@users.noreply.github.com>
2024-08-29 11:08:41 +0530

    .env

--- constants/index.ts
+++ constants/index.ts
@@ -1,14 +1,10 @@
-export const projectId =
-    "BI5-250tyqwU_79yFve_chx6hiE-f8iCxPHe0oqDpv-xU9dvGJ1p3JLo1y0AqzlMKDoZ_w0NLjxIFyNhxXJ6L6Y";
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 export const oauthClientId =
-    process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID || "1037067592138-peac48ld0g4r8inm8udvbo0bupvgpkf9.apps.googleusercontent.com";
+    process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID;
 export const web3AuthVerifier = "hyper-google-verifier";
 export const web3AuthLoginType = "google";
 export const web3AuthClientId =
-    process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID || "BHQMa4DE2EnG4qb66kotFkN1fzA4S-ZRhVOr9TzaWIqeCHPp1mVWgNM0lyfjA-K1E4dErw6xoCC7spCeMylxmIs";
+    process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID;
 export const productName = "Hyper";
 export const rainbowKitProjectId = "fb3037b60ba3165d90a7f1bb1a727cc5";
-export enum LOGGED_IN {
-    GOOGLE = "google",
-    EXTERNAL_WALLET = "external_wallet",
-}
+
