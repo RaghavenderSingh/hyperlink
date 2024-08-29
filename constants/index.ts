6d00bceb5d41d3a41fb45a0c940b6a5fae99aa12
Raghavender Singh <92844911+RaghavenderSingh@users.noreply.github.com>
2024-08-29 11:32:57 +0530

    fix

--- constants/index.ts
+++ constants/index.ts
@@ -3,8 +3,7 @@ export const oauthClientId =
     process.env.NEXT_PUBLIC_OAUTH_CLIENT_ID;
 export const web3AuthVerifier = "hyper-google-verifier";
 export const web3AuthLoginType = "google";
-export const web3AuthClientId =
-    process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID;
+export const web3AuthClientId = process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID || "BHQMa4DE2EnG4qb66kotFkN1fzA4S-ZRhVOr9TzaWIqeCHPp1mVWgNM0lyfjA-K1E4dErw6xoCC7spCeMylxmIs";
 export const productName = "Hyper";
 export const rainbowKitProjectId = "fb3037b60ba3165d90a7f1bb1a727cc5";
 
